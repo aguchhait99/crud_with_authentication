29cb050f5559c89bb1fc1045583c14d4ab32dc50
aguchhait99 <avikguchhait09@gmail>
2023-10-08 00:11:13 +0530

    lk

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Routing from './router/Routing';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
+      <ToastContainer/>
       <Routing/>
     </>
   );
